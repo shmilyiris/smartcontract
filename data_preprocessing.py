9f07955aab83ee9fe1b813aa318e9444695d66a3
Zhigao Geng <zhigaogeng.johnson@gmail.com>
2023-11-27 14:24:13 +0800

    [MODIFY] data_preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,8 +7,9 @@ import pandas as pd
 available_versions = ['0.5.0', '0.5.1', '0.5.2', '0.5.3', '0.5.4', '0.5.5', '0.5.6', '0.5.7', '0.5.8', '0.5.9',
                       '0.5.10', '0.5.11', '0.5.12', '0.5.13', '0.5.14', '0.5.15', '0.5.16', '0.5.17',
                       '0.6.0', '0.6.1', '0.6.2', '0.6.3', '0.6.4', '0.6.5', '0.6.6', '0.6.7', '0.6.8', '0.6.9',
-                      '0.6.10', '0.6.11', '0.6.12', '0.7.0', '0.7.6',
-                      '0.8.2', '0.8.3', '0.8.4', '0.8.5', '0.8.6', '0.8.7', '0.8.8', '0.8.9', '0.8.10',
+                      '0.6.10', '0.6.11', '0.6.12',
+                      '0.7.0', '0.7.1', '0.7.2', '0.7.3', '0.7.4', '0.7.5', '0.7.6',
+                      '0.8.0', '0.8.1', '0.8.2', '0.8.3', '0.8.4', '0.8.5', '0.8.6', '0.8.7', '0.8.8', '0.8.9', '0.8.10',
                       '0.8.11', '0.8.12', '0.8.13', '0.8.14', '0.8.15', '0.8.16', '0.8.17', '0.8.18',
                       '0.8.19', '0.8.20', '0.8.21', '0.8.22', '0.8.23']
 
@@ -42,7 +43,7 @@ def preprocess(df):
                 # solidity version definition position
                 def_pos = res.find('pragma solidity')
                 if def_pos != -1:
-                    version_demand = res[def_pos + len('pragma solidity'):-1]
+                    version_demand = res[def_pos + len('pragma solidity'):]
                     versions = re.findall('\d+\.\d+\.\d+', version_demand)
                     version = get_version(versions)
                     if version == -1:
@@ -52,6 +53,8 @@ def preprocess(df):
                             f.write(sc)
                         with open(f'./asm_final/source/versions/{addr}.txt', 'w') as f:
                             f.write(version)
+                        with open(f'./asm_final/source/bc/{addr}.bin', 'w') as f:
+                            f.write(bc)
                         os.system(f'solc-select use {version}')
                         print(os.system(f'solc --asm ./asm_final/source/sol/{addr}.sol > ./asm_final/source/asm/{addr}.asm'))
                         print(os.system(f'solc --asm --optimize ./asm_final/source/sol/{addr}.sol > ./asm_final/source/asm/{addr}.asm'))
@@ -61,5 +64,6 @@ def preprocess(df):
 
 if __name__ == '__main__':
     dataset_path = './dataset'
-    for file in os.listdir(dataset_path):
-        df = pd.read_parquet(file)
+    for fileName in os.listdir(dataset_path):
+        df = pd.read_parquet(os.path.join(dataset_path, fileName))
+        preprocess(df)
---
+++ scripts/DelAllSourceData.sh
@@ -0,0 +1,5 @@
+rm ~/bytecode_similarity/smartcontract/asm_final/source/asm/*
+rm ~/bytecode_similarity/smartcontract/asm_final/source/opt/*
+rm ~/bytecode_similarity/smartcontract/asm_final/source/sol/*
+rm ~/bytecode_similarity/smartcontract/asm_final/source/bc/*
+rm ~/bytecode_similarity/smartcontract/asm_final/source/versions/*
